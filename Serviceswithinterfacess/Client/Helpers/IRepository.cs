432cb781175a146da6849e2cbf6eefc0bd1d4c3d
YATENDRA AWANA <yaten96@live.com>
2021-07-17 17:28:42 +0530

    Add project files.

---
+++ Serviceswithinterfacess/Client/Helpers/IRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Serviceswithinterfacess.Client.Helpers
+{
+   public  interface IRepository
+    {
+        List<Data> GetData();
+    }
+}
---
+++ Serviceswithinterfacess/Client/Helpers/MyClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Serviceswithinterfacess.Client.Helpers
+{
+    public class MyClass : IRepository
+    {
+        public List<Data> GetData()
+        {
+           
+            return new List<Data>()
+            {
+                new Data(){Id=101,Name="Aman", Address="Noida"},
+                new Data(){Id=102,Name="manish", Address="Gr Noida"},
+                new Data(){Id=103,Name="Ajay", Address="Noida"},
+                new Data(){Id=104,Name="Vijay", Address="Gr Noida"}
+            };
+        }
+    }
+}
